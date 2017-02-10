81f61e5ded04eaf25cfa039ead8b2bce2ade642d
Kamil Buransky <kamil.buransky@prism-dm.co.uk>
2017-02-10 11:32:21 +0100

    new files

---
+++ src/RegularExpressionGroupTest.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by kamilb on 10.2.2017.
+ */
+public class RegularExpressionGroupTest
+{
+    @Test(groups = { "include-test-one" })
+    public void testMethodOne() {
+        System.out.println("Test method one");
+    }
+
+    @Test(groups = { "include-test-two" })
+    public void testMethodTwo() {
+        System.out.println("Test method two");
+    }
+
+    @Test(groups = { "test-one-exclude" })
+    public void testMethodThree() {
+        System.out.println("Test method three");
+    }
+
+    @Test(groups = { "test-two-exclude" })
+    public void testMethodFour() {
+        System.out.println("Test method Four");
+    }
+}
